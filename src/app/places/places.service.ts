fa3cfc4a63a6d31a384c3cd000ac6302f9574749
Enrico Giurin <enricogiurin@gmail.com>
2025-01-14 22:14:38 +0100

    started improved version of optimistic update

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -25,10 +25,17 @@ export class PlacesService {
   }
 
   addPlaceToUserPlaces(place: Place) {
-    this.userPlaces.update(prevPlaces => [...prevPlaces, place]);
+    const prevPlaces = this.userPlaces();
+    this.userPlaces.set([...prevPlaces, place]);
     return this.httpClient.put('http://localhost:3000/user-places', {
       placeId: place.id
-    })
+    }).pipe(
+      catchError(error => {
+        //in case of error we rollback to the prev state.
+        this.userPlaces.set(prevPlaces);
+        return throwError(() => new Error('failed to store selected place'))
+      })
+    )
   }
 
   removeUserPlace(place: Place) {}
